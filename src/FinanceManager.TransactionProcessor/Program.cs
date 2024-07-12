f77e0970a97c6d2aaa71971caddde6a07183b84b
Aidan Langelaan <aidanlangelaan@gmail.com>
2024-07-12 22:34:39 +0200

    Added serilog to worker

--- src/FinanceManager.TransactionProcessor/Program.cs
+++ src/FinanceManager.TransactionProcessor/Program.cs
@@ -1,7 +1,40 @@
 using FinanceManager.TransactionProcessor;
+using Serilog;
+using Serilog.Events;
+using Serilog.Templates;
+using Serilog.Templates.Themes;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+try
+{
+    var builder = Host
+        .CreateDefaultBuilder(args)
+        .UseSerilog((context, configuration) =>
+        {
+            configuration.ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(new ExpressionTemplate(
+                    // Include trace and span ids when present.
+                    "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}",
+                    theme: TemplateTheme.Code));
+        });
+    
+    builder.ConfigureAppConfiguration((_, config) =>
+    {
+        config.AddJsonFile($"appsettings.local.json", optional: true);
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    });
 
-var host = builder.Build();
-await host.RunAsync();
+    await builder.Build().RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occurred during bootstrapping");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
--- src/FinanceManager.TransactionProcessor/Worker.cs
+++ src/FinanceManager.TransactionProcessor/Worker.cs
@@ -1,22 +1,16 @@
 namespace FinanceManager.TransactionProcessor;
 
-public class Worker : BackgroundService
+public class Worker(ILogger<Worker> logger) : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
-            }
+            logger.LogInformation("Worker started running at: {Time}", DateTimeOffset.Now);
+            
+            
+            
+            logger.LogInformation("Worker completed running at: {Time}", DateTimeOffset.Now);
 
             await Task.Delay(1000, stoppingToken);
         }
