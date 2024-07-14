1928adee0065621f8ed88b9f8dd753da1f1c88b4
Aidan Langelaan <aidanlangelaan@gmail.com>
2024-07-14 14:50:23 +0200

    Category migration

---
+++ src/FinanceManager.Data/Migrations/20240714124129_ImplementCategoryTable.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FinanceManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("1896c355-6550-4f28-ae63-176073b9454d"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("c4007b7e-a94e-44b5-b1ef-c05f39030899"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("f0110143-a9d8-4602-83ac-ffe54fa32188"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "", "Uncategorized" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("35541920-f7e1-448d-9e08-4220c03eaebf"), null, "Admin", "ADMIN" },
+                    { new Guid("4ba94ced-1d0d-49cd-8534-604b3373465f"), null, "SystemAdmin", "SYSTEMADMIN" },
+                    { new Guid("f76f491d-b6b5-42df-81ed-2f3712bdafac"), null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("35541920-f7e1-448d-9e08-4220c03eaebf"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("4ba94ced-1d0d-49cd-8534-604b3373465f"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("f76f491d-b6b5-42df-81ed-2f3712bdafac"));
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Categories");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Description",
+                keyValue: null,
+                column: "Description",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: "Uncategorized");
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("1896c355-6550-4f28-ae63-176073b9454d"), null, "SystemAdmin", "SYSTEMADMIN" },
+                    { new Guid("c4007b7e-a94e-44b5-b1ef-c05f39030899"), null, "Admin", "ADMIN" },
+                    { new Guid("f0110143-a9d8-4602-83ac-ffe54fa32188"), null, "User", "USER" }
+                });
+        }
+    }
+}
